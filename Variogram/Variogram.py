ad6c0a9b37d7bc083bcb595ec4c76e2e1183d647
RealYomi <79029759+RealYomi@users.noreply.github.com>
2025-06-12 10:18:07 +0200

    Update Variogram.py

--- Variogram/Variogram.py
+++ Variogram/Variogram.py
@@ -1,7 +1,11 @@
 
 # %%
 """
-Chat
+This Code can be used to estimate both structured and unstructured Variograms
+from Experimental data.
+Enter the lag steps and tolerance.
+Enter the number of directions and tolerance.
+Enter the initial direction (angle)
 """
 import numpy as np
 import pandas as pd
